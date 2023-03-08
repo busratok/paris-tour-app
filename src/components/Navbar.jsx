2591da5ef80d08d92c6d136bdfa83356a901878b
busratok <busratokuc@gmail.com>
2023-03-08 21:48:14 +0000

    Typing error is fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <a href="#home">Home</a>
-      <a href="#tour-places">Attactions</a>
+      <a href="#tour-places">Attractions</a>
       <a href="#blog">Blog</a>
       <a href="#contact">Contact</a>
     </div>
